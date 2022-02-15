fd5300d09d5c92be7005dd0d75d3471447b7a4a4
jdj0 <joshua.davidjones92@gmail.com>
2022-02-15 09:26:38 +0000

    add JS selectors

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("Test");
+//Selectors
+const toDoInput = document.querySelector(".todo-input");
+const toDoButton = document.querySelector(".todo-button");
+const toDoList = document.querySelector(".todo-list");
+
+//Events Listeners
+toDoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(event){
+    //Prevents from from submitting
+    event.preventDefault()
+    console.log("hello!")
+}
