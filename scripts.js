3d2b3b1d84da3aa225752d10196b2dd0f2594efd
jdj0 <joshua.davidjones92@gmail.com>
2022-02-15 22:09:26 +0000

    Add JS function to create to-do li in html

--- scripts.js
+++ scripts.js
@@ -10,5 +10,24 @@ toDoButton.addEventListener("click", addTodo);
 function addTodo(event){
     //Prevents from from submitting
     event.preventDefault()
-    console.log("hello!")
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo")
+    //Create LI
+    const newToDo = document.createElement("li");
+    newToDo.innerText = "Hey";
+    newToDo.classList.add("todo-item");
+    todoDiv.appendChild(newToDo);
+    //Check off button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = "<i class = 'fa-solid fa-square-plus'></i>";
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    toDoList.appendChild(todoDiv);
 }
