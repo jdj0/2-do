1019cf32a8f3cda1f0f3be14c7817f2d76ea8637
jdj0 <joshua.davidjones92@gmail.com>
2022-02-16 22:12:08 +0000

    Add css to generated todo li

--- scripts.js
+++ scripts.js
@@ -15,19 +15,21 @@ function addTodo(event){
     todoDiv.classList.add("todo")
     //Create LI
     const newToDo = document.createElement("li");
-    newToDo.innerText = "Hey";
+    newToDo.innerText = toDoInput.value;
     newToDo.classList.add("todo-item");
     todoDiv.appendChild(newToDo);
     //Check off button
     const completedButton = document.createElement("button");
-    completedButton.innerHTML = "<i class = 'fa-solid fa-square-plus'></i>";
+    completedButton.innerHTML = "<i class='fa-solid fa-square-check'></i>";
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
     //Trash button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     //Append to list
     toDoList.appendChild(todoDiv);
+    //Clear Todo input value
+    toDoInput.value = "";
 }
